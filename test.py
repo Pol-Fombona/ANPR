76b58606f56a372cbc444c50d8ec34a3ed4176be
afunes99 <99901616+afunes99@users.noreply.github.com>
2022-09-27 16:39:10 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -178,16 +178,12 @@ def metode_B(image):
 
     cv2.imshow('pepe', new_image)
     cv2.waitKey(0)
-
-    (x, y) = np.where(mask==255)
-    (x1, y1) = (np.min(x), np.min(y))
-    (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = new_image[x1:x2+1, y1:y2+1]
-
-    cv2.imshow('final image', cropped_image)
+    
+    warped = homography(new_image,location)
+    cv2.imshow("warped",warped)
     cv2.waitKey(0)
+    return warped
 
-    return cropped_image
 
 
 def read_number_plate(image):
@@ -210,7 +206,37 @@ def elimina_e(img, thr):
 
   return img_ret
 
+def order_points(pts):
+	rect = np.zeros((4, 2), dtype = "float32")
 
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+
+	return rect
+
+def homography(gray_img,location):
+	src = order_points(np.squeeze(location).astype(np.float32))
+	(tl, tr, br, bl) = src
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	M = cv2.getPerspectiveTransform(src, dst) 
+	warped = cv2.warpPerspective(gray_img, M, (maxWidth, maxHeight))
+	return warped
 
 if __name__ == '__main__':
 
