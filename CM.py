2b2a5681b77c7502459b2f5bb79505b0955e9a29
Victor Garcia <viic_94@hotmail.es>
2022-10-09 21:44:19 +0200

    Change file

--- CM.py
+++ CM.py
@@ -2,12 +2,26 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_unique(y):
+    y = np.array(y)
+    return np.unique(y)
 
 
 # Display the Confusion Matrix
 def display_CM(y_pred, y_true):
 
-    cm = confusion_matrix(y_true, y_pred)
-    disp = ConfusionMatrixDisplay(cm)
+    labels = get_unique(y_pred + y_true)
+
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    disp = ConfusionMatrixDisplay(cm, display_labels=labels)
     disp.plot()
+
+    disp.ax_.set(
+        title='Confusion Matrix',
+        xlabel='Predicted',
+        ylabel='Actual')
+
     plt.show()
